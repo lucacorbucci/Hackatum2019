fc53468f0e49e3efdea926980b1a1cb96168818f
lucacorbucci <tankado@live.it>
2019-11-24 02:34:02 +0100

    Data visualization

---
+++ Visualization/server.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const server = require('http').createServer(function (req, res) {
+  // Set CORS headers
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Request-Method', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  if (req.method === 'OPTIONS') {
+    res.writeHead(200);
+    res.end();
+    return;
+  }
+});
+
+server.on('request', (req, res) => {
+  try {
+    const src = fs.createReadStream('./data.json');
+    src.pipe(res);
+  }
+  catch (e) {
+
+  }
+
+});
+
+server.listen(8000);
